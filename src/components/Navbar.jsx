6c07cd281e62b2693b2b4f1f230f5461a27ed3b4
nsunklodas <121260132+nsunklodas@users.noreply.github.com>
2024-01-21 00:11:31 +0100

    Add files via upload

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import logo from '../assets/logo.jpg'
+
+
+
+const Navbar = () => {
+    return (
+      <nav>
+        <img className='logo' src={logo} alt="logo" />
+      </nav>
+    )
+  }
+  
+export default Navbar
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Navbar from './components/Navbar.jsx'
+import './index.css'
+
+
+let root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+
+    <Navbar />
+)
